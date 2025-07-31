f586ec00b196af3637cc427ee91e4152ec756b7d
Piotr Puchala <piotr.puchala89@gmail.com>
2025-07-31 18:28:52 +0200

    feat: added tests for the dashboard for the new user

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import "dayjs/locale/pl";
 import { test, expect } from "next/experimental/testmode/playwright/msw";
 import { handlers } from "./mocks/client";
-import { existingUser, UserToken } from "./constants/user";
+import { existingUser, newUser, UserToken } from "./constants/user";
 import { categoryLabels } from "./constants/transaction";
 import { monthlyTransactionsExistingUser } from "./mocks/data/monthlyTransactions";
 import { yearlyTransactionsExistingUser } from "./mocks/data/yearlyTransactions";
@@ -657,6 +657,285 @@ test.describe("Dashboard - existing user", () => {
       for (let i = 0; i < incomesRowsCount; i++) {
         await expect(rows.nth(i)).not.toContainText("Wydatek");
       }
+    });
+  });
+});
+
+test.describe("Dashboard - new user", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.context().addCookies([
+      {
+        name: "auth_token",
+        value: UserToken.NEW_USER_TOKEN,
+        domain: "localhost",
+        path: "/",
+        httpOnly: false,
+        secure: false,
+        sameSite: "Lax",
+      },
+    ]);
+
+    await page.goto("/dashboard");
+  });
+
+  test.describe("Check header", () => {
+    test("shows app logo, title and avatar", async ({ page }) => {
+      await expect(page.getByTestId("header-logo")).toBeVisible();
+      await expect(page.getByTestId("header-title")).toBeVisible();
+      await expect(page.getByTestId("header-title")).toHaveText("MojeFinanse");
+      await expect(page.getByTestId("header-avatar-icon")).toBeVisible();
+      await expect(
+        page.getByText(`Witaj ${newUser.name} ${newUser.surname}`)
+      ).toBeVisible();
+    });
+  });
+
+  test.describe("Check transaction input section", () => {
+    test("shows transaction input section", async ({ page }) => {
+      await expect(page.getByTestId("transaction-input-field")).toBeVisible();
+
+      const infoIcon = page.getByTestId("transaction-info-icon");
+      await expect(infoIcon).toBeVisible();
+
+      await infoIcon.click();
+
+      const infoAlert = page.getByTestId("transaction-info-alert");
+      await expect(infoAlert).toBeVisible();
+      await expect(infoAlert).toHaveText(
+        "Wpisz swój wydatek lub wpływ w formie opisowej. Nasza aplikacja zinterpretuje wprowadzony tekst i automatycznie uzupełni formularz. Możesz także dodać zdjęcie paragonu, a my odczytamy z niego dane. Jeśli wolisz – kliknij „Dodaj transakcję” i wypełnij formularz ręcznie."
+      );
+
+      await page.mouse.click(10, 10);
+      await expect(infoAlert).not.toBeVisible();
+
+      const addTransactionButton = page.getByTestId("transaction-add-button");
+      await expect(addTransactionButton).toBeVisible();
+      await expect(addTransactionButton).toHaveText("Dodaj transakcję");
+
+      const addImageButton = page.getByTestId("transaction-add-image-button");
+      await expect(addImageButton).toBeVisible();
+      await expect(addImageButton).toHaveText("Dodaj zdjęcie");
+
+      await expect(
+        page.getByTestId("transaction-add-image-icon")
+      ).toBeVisible();
+    });
+
+    test("shows add transaction modal", async ({ page }) => {
+      await page.getByTestId("transaction-add-button").click();
+      await expect(page.getByTestId("transaction-add-modal")).toBeVisible();
+      await expect(page.getByText("Wprowadź transakcję")).toBeVisible();
+      await expect(page.getByText("Rodzaj *")).toBeVisible();
+
+      const expenseButton = page.getByTestId(
+        "transaction-add-modal-expense-button"
+      );
+      await expect(expenseButton).toBeVisible();
+      await expect(expenseButton).toHaveText("Wydatek");
+
+      const incomeButton = page.getByTestId(
+        "transaction-add-modal-income-button"
+      );
+      await expect(incomeButton).toBeVisible();
+      await expect(incomeButton).toHaveText("Wpływ");
+
+      await expect(
+        page.getByTestId("transaction-add-modal-amount-field")
+      ).toBeVisible();
+
+      const amountFieldCurrency = page.getByTestId(
+        "transaction-add-modal-amount-currency"
+      );
+      await expect(amountFieldCurrency).toBeVisible();
+      await expect(amountFieldCurrency).toHaveText("zł");
+
+      await expect(
+        page.getByTestId("transaction-add-modal-date-input")
+      ).toBeVisible();
+      await expect(page.getByTestId("CalendarIcon")).toBeVisible();
+
+      const categoryLabel = page.getByTestId(
+        "transaction-add-modal-category-label"
+      );
+      await expect(categoryLabel).toBeVisible();
+      await expect(categoryLabel).toHaveText("Kategoria *");
+
+      const categorySelect = page.getByTestId(
+        "transaction-add-modal-category-select"
+      );
+      await expect(categorySelect).toBeVisible();
+      await categorySelect.click();
+
+      for (const label of Object.values(categoryLabels)) {
+        await expect(
+          page.getByTestId(
+            `transaction-add-modal-category-item-${label.toLowerCase()}`
+          )
+        ).toBeVisible();
+      }
+
+      await page.keyboard.press("Escape");
+      await expect(page.getByRole("listbox")).not.toBeVisible();
+
+      await expect(
+        page.getByTestId("transaction-add-modal-description-field")
+      ).toBeVisible();
+
+      const maxLength = page.getByTestId(
+        "transaction-add-modal-description-max-length"
+      );
+      await expect(maxLength).toBeVisible();
+      await expect(maxLength).toHaveText("0/200");
+
+      const cancelButton = page.getByTestId(
+        "transaction-add-modal-cancel-button"
+      );
+      await expect(cancelButton).toBeVisible();
+      await expect(cancelButton).toContainText("Anuluj");
+
+      const saveButton = page.getByTestId("transaction-add-modal-save-button");
+      await expect(saveButton).toBeVisible();
+      await expect(saveButton).toContainText("Zapisz");
+    });
+  });
+
+  test.describe("Check empty transactions charts", () => {
+    test("shows disabled transaction type buttons", async ({ page }) => {
+      await expect(
+        page.getByText("Podsumowanie Twoich finansów:")
+      ).toBeVisible();
+
+      const expenseButton = page.getByTestId("transaction-type-expense-button");
+      await expect(expenseButton).toBeVisible();
+      await expect(expenseButton).toContainText("Wydatki");
+      await expect(expenseButton).toBeDisabled();
+
+      const incomeButton = page.getByTestId("transaction-type-income-button");
+      await expect(incomeButton).toBeVisible();
+      await expect(incomeButton).toContainText("Wpływy");
+      await expect(incomeButton).toBeDisabled();
+    });
+
+    test("shows empty monthly transactions chart", async ({ page }) => {
+      await expect(
+        page.getByTestId("monthly-empty-chart-container")
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice").nth(0)
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice-icon").nth(0)
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice-text").nth(0)
+      ).toContainText(
+        "Dodaj swoją pierwszą transakcję, aby zobaczyć miesięczne podsumowanie wydatków i wpływów."
+      );
+      await expect(page.getByTestId("monthly-chart-bar")).toHaveCount(0);
+    });
+
+    test("shows empty yearly transactions chart", async ({ page }) => {
+      await expect(
+        page.getByTestId("yearly-empty-chart-container")
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice").nth(1)
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice-icon").nth(1)
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice-text").nth(1)
+      ).toContainText(
+        "Dodaj swoją pierwszą transakcję, aby zobaczyć roczne podsumowanie wydatków i wpływów."
+      );
+      await expect(page.getByTestId("monthly-chart-bar")).toHaveCount(0);
+    });
+  });
+
+  test.describe("Check empty transaction history table", () => {
+    test("shows disabled filter buttons", async ({ page }) => {
+      await expect(page.getByText("Historia transakcji:")).toBeVisible();
+
+      const allTransactionsButton = page.getByTestId(
+        "transaction-history-all-button"
+      );
+      await expect(allTransactionsButton).toBeVisible();
+      await expect(allTransactionsButton).toHaveText("Wszystkie");
+      await expect(allTransactionsButton).toBeDisabled();
+
+      const expensesButton = page.getByTestId(
+        "transaction-history-expense-button"
+      );
+      await expect(expensesButton).toBeVisible();
+      await expect(expensesButton).toHaveText("Wydatki");
+      await expect(expensesButton).toBeDisabled();
+
+      const incomesButton = page.getByTestId(
+        "transaction-history-income-button"
+      );
+      await expect(incomesButton).toBeVisible();
+      await expect(incomesButton).toHaveText("Wpływy");
+      await expect(incomesButton).toBeDisabled();
+    });
+
+    test("shows empty transaction history table", async ({ page }) => {
+      await expect(
+        page.getByTestId("transaction-history-empty-table")
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("transaction-history-empty-table-header")
+      ).toBeVisible();
+
+      const tableHeaderDate = page.getByTestId(
+        "transaction-history-empty-table-header-date"
+      );
+      await expect(tableHeaderDate).toBeVisible();
+      await expect(tableHeaderDate).toHaveText("Data");
+
+      const tableHeaderAmount = page.getByTestId(
+        "transaction-history-empty-table-header-amount"
+      );
+      await expect(tableHeaderAmount).toBeVisible();
+      await expect(tableHeaderAmount).toHaveText("Kwota");
+
+      const tableHeaderType = page.getByTestId(
+        "transaction-history-empty-table-header-type"
+      );
+      await expect(tableHeaderType).toBeVisible();
+      await expect(tableHeaderType).toHaveText("Rodzaj");
+
+      const tableHeaderCategory = page.getByTestId(
+        "transaction-history-empty-table-header-category"
+      );
+      await expect(tableHeaderCategory).toBeVisible();
+      await expect(tableHeaderCategory).toHaveText("Kategoria");
+
+      const tableHeaderDescription = page.getByTestId(
+        "transaction-history-empty-table-header-description"
+      );
+      await expect(tableHeaderDescription).toBeVisible();
+      await expect(tableHeaderDescription).toHaveText("Opis");
+
+      const rows = page.getByTestId("transaction-history-empty-table-row");
+      await expect(rows).toHaveCount(5);
+
+      const showMoreButton = page.getByTestId(
+        "transaction-history-show-more-button"
+      );
+      await expect(showMoreButton).not.toBeVisible();
+
+      await expect(
+        page.getByTestId("empty-transactions-notice").nth(2)
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice-icon").nth(2)
+      ).toBeVisible();
+      await expect(
+        page.getByTestId("empty-transactions-notice-text").nth(2)
+      ).toContainText(
+        "Dodaj swoją pierwszą transakcję, aby zobaczyć historię wydatków i wpływów."
+      );
     });
   });
 });
